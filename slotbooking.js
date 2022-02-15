1eaef8f6ea9aed0aec7c938202224f72b2723387
AdhikChintalwar <AdhikChintalwar@github.com>
2022-02-15 12:34:36 +0530

    html slot booking

---
+++ slotbooking.js
@@ -0,0 +1,28 @@
+const dummyData = [
+    {
+      date: '14-2-2022',
+      Avltime: '7:30',
+      Avl: true
+    },
+    {
+      date: '12-2-2022',
+      Avltime: '6:30',
+      Avl: true
+    },
+    {
+      date: '12-2-2022',
+      Avltime: '7:30',
+      Avl: false
+    },
+    {
+      date: '14-2-2022',
+      Avltime: '8:30',
+      Avl: false
+    },
+    {
+      date: '13-2-2022',
+      Avltime: '8:30',
+      Avl: true
+    }
+
+  ]
